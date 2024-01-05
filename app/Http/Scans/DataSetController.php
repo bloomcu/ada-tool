6fbefa38b3e4d7b59bbe42a156554df36c719208
Bryan <bryan.e.dev@gmail.com>
2024-01-05 17:41:36 -0600

    FIX : add route/model binding to dataset

--- app/Http/Scans/DataSetController.php
+++ app/Http/Scans/DataSetController.php
@@ -6,20 +6,14 @@ use Illuminate\Http\Request;
 use DDD\Domain\Base\Organizations\Organization;
 use DDD\App\Services\Apify\ApifyInterface;
 use DDD\App\Controllers\Controller;
+use DDD\Domain\Base\Evaluations\Evaluation;
 
 class DataSetController extends Controller
 {
-    public function dataset(Request $request, ApifyInterface $service)
+    public function dataset(Request $request, ApifyInterface $service, Evaluation $evaluation)
     {
-        $actor = $service->getDataset($request->dataset_id);
-
-        // $scan = $organization->scans()->create([
-        //     'url'        => $request->url,
-        //     'run_id'     => $actor['run_id'],
-        //     'queue_id'   => $actor['queue_id'],
-        //     'dataset_id' => $actor['dataset_id'],
-        // ]);
-
+        
+        $actor = $service->getDataset($evaluation->dataset_id);
         return response()->json([
             'message' => 'Results:',
             // 'data' => $scan
