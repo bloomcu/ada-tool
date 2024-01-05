255b32d39e9e5af54f01528ed531b55030f9becc
Bryan <bryan.e.dev@gmail.com>
2024-01-05 14:29:42 -0600

    FEATURE : ADD migrations and patch rollback on users and media tables

--- database/migrations/2022_07_02_063931_create_media_table.php
+++ database/migrations/2022_07_02_063931_create_media_table.php
@@ -34,4 +34,8 @@ return new class extends Migration
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
+    public function down()
+    {
+        Schema::dropIfExists('media');
+    }
 };
