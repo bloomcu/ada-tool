a72bb99207e94997efbf1432f0a094396753aa74
Bryan <bryan.e.dev@gmail.com>
2024-01-05 17:05:52 -0600

    FIX : scan controller, create Evaluation record on success

--- app/Http/Scans/ScanController.php
+++ app/Http/Scans/ScanController.php
@@ -6,11 +6,13 @@ use Illuminate\Http\Request;
 use DDD\Domain\Base\Organizations\Organization;
 use DDD\App\Services\Apify\ApifyInterface;
 use DDD\App\Controllers\Controller;
+use DDD\Domain\Base\Evaluations\Evaluation;
 
 class ScanController extends Controller
 {
-    public function store(Request $request, ApifyInterface $service)
+    public function store(Request $request, ApifyInterface $service, Evaluation $evaluation)
     {
+        $evaluation->site_id = 1;
         $actor = $service->runActor($request->url);
 
         // $scan = $organization->scans()->create([
@@ -19,11 +21,20 @@ class ScanController extends Controller
         //     'queue_id'   => $actor['queue_id'],
         //     'dataset_id' => $actor['dataset_id'],
         // ]);
-
-        return response()->json([
-            'message' => 'Scan in progress',
-            // 'data' => $scan
-            'data' => $actor
-        ]);
+        $evaluation->run_id = $actor['run_id'];
+        $evaluation->queue_id = $actor['queue_id'];
+        $evaluation->dataset_id = $actor['dataset_id'];
+        
+        if($evaluation->save()) {
+            return response()->json([
+                'message' => 'Scan in progress',
+                // 'data' => $scan
+                'data' => [
+                    'evaluation'    =>$evaluation,
+                    'actor'         =>$actor
+                ]
+            ]);
+        }
+        return false;
     }
 }
