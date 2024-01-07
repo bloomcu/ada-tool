64a259360c571b3b26ea1a0ad30b60cbfcf30bf3
Ryan Harmon <ryan@bloomcu.com>
2024-01-07 09:09:17 -0700

    FEATURE: Connect scans to organizations

--- app/Domain/Organizations/Organization.php
+++ app/Domain/Organizations/Organization.php
@@ -3,6 +3,7 @@
 namespace DDD\Domain\Organizations;
 
 use DDD\Domain\Sites\Site;
+use DDD\Domain\Scans\Scan;
 use DDD\Domain\Base\Organizations\Organization as BaseOrganization;
 
 class Organization extends BaseOrganization
@@ -15,5 +16,15 @@ class Organization extends BaseOrganization
     public function sites()
     {
         return $this->hasMany(Site::class);
+    }
+
+    /**
+     * Scans associated with this organization.
+     *
+     * @return hasMany
+     */
+    public function scans()
+    {
+        return $this->hasMany(Scan::class);
     }
 }
