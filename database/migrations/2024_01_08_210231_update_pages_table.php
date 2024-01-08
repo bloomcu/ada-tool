da829bdcda90e7eca7d43fcf4df89c00059aefc8
Ryan Harmon <ryan@bloomcu.com>
2024-01-08 14:56:16 -0700

    FIX: Replace evaluation_id with scan_id on pages

---
+++ database/migrations/2024_01_08_210231_update_pages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            $table->foreignId('scan_id')->after('evaluation_id');
+
+            // Foreign constraints
+            $table->foreign('scan_id')->references('id')->on('scans');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            $table->dropForeign(['scan_id']);
+            $table->dropColumn('scan_id');
+        });
+    }
+};
