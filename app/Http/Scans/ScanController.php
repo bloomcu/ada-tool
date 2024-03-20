cf432dd5e1ed9b1256a158301d467c047d62e178
Bryan <bryan.e.dev@gmail.com>
2024-03-20 17:57:30 -0500

    FIX : revert ScanController changes

--- app/Http/Scans/ScanController.php
+++ app/Http/Scans/ScanController.php
@@ -6,7 +6,6 @@ use DDD\Domain\Scans\Resources\ScanResource;
 use DDD\Domain\Scans\Scan;
 use DDD\Domain\Organizations\Organization;
 use DDD\App\Controllers\Controller;
-use Illuminate\Support\Facades\Log;
 
 class ScanController extends Controller
 {
@@ -19,9 +18,7 @@ class ScanController extends Controller
 
     public function show(Organization $organization, Scan $scan)
     {
-        ini_set('memory_limit', '2048M');
-        $resource = new ScanResource($scan->load('pages'));
-        // Log::info('usage_old' . memory_get_usage(true));
-        return $resource;
+        
+        return new ScanResource($scan->load('pages'));
     }
 }
