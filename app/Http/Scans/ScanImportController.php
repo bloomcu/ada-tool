649cb741bdfd57d3237d80d1dc84bcddd4a10dc8
Bryan <bryan.e.dev@gmail.com>
2024-03-20 17:55:50 -0500

    FIX : optimize import function

--- app/Http/Scans/ScanImportController.php
+++ app/Http/Scans/ScanImportController.php
@@ -13,77 +13,87 @@ class ScanImportController extends Controller
 {
     public function import(Organization $organization, Scan $scan, ApifyInterface $apifyService)
     {
-        // ini_set('memory_limit', '1024M');
-        $dataset = $apifyService->getDataset($scan->dataset_id);
+        // ini_set('memory_limit', '256M');
+        $chunk_size = 20;
+        $dataset_meta = $apifyService->getDataSetMeta($scan->dataset_id);
+        $dataset_count = $dataset_meta['itemCount'];
+        $page_count = floor($dataset_count/$chunk_size);
         
-        $dataset = json_decode($dataset, true);
         // These numbers include all elements and all tests these are not unique and may be useful in tracking global issues 
         $total_violations = 0;
         $total_warnings = 0;
-        
-
-        // Tallys number of pages with errors/warnings
         $violations_pages = 0;
         $warnings_pages = 0;
-        $pages = [];
-        foreach ($dataset as $entry) {
-            $results = json_decode($entry['results'], true);
+      
+        $collected_page_count = 0;
+        // Fetch the data set in chunks
+        for($i = 1; $i<=$page_count; $i++) {
+            $dataset = $apifyService->getDataset($scan->dataset_id, 20, $i);
+            // Log::info('usage_get: ' . memory_get_peak_usage(true));
+            $dataset = json_decode($dataset, true);
+            // Log::info('usage_decode: ' . memory_get_peak_usage(true));
             
-            $page_has_violations = false;
-            $page_has_warnings = false;
-            // Tally elements in violation for a specific page
-            $violations_count_this_page = 0;
-            $warnings_count_this_page = 0;
-            foreach($results['rule_results'] as
-                [
-                    "elements_violation" => $elements_violation,
-                    "elements_warning"=> $elements_warning
-                ]
-            ){       
-                $total_violations += $elements_violation;
-                $violations_count_this_page += $elements_violation;
+            foreach ($dataset as $entry) {
+                $pages = [];
+                $results = json_decode($entry['results'], true);
+                
+                $page_has_violations = false;
+                $page_has_warnings = false;
+                // Tally elements in violation for a specific page
+                $violations_count_this_page = 0;
+                $warnings_count_this_page = 0;
+                foreach($results['rule_results'] as
+                    [
+                        "elements_violation" => $elements_violation,
+                        "elements_warning"=> $elements_warning
+                    ]
+                ){       
+                    $total_violations += $elements_violation;
+                    $violations_count_this_page += $elements_violation;
+    
+                    if($elements_violation > 0 && $page_has_violations == false){
+                        $page_has_violations = true;
+                        
+                    }
+                    
+                    $total_warnings += $elements_warning;
+                    $warnings_count_this_page += $elements_warning;
+                    
+                    if($elements_warning > 0 && $page_has_warnings == false){
+                        $page_has_warnings = true;
+                        
+                    }
+                }
+                if($page_has_violations) {
+                    $violations_pages++;
+                }
+                if($page_has_warnings) {
+                    $warnings_pages++;
+                }
+                $pages[] = [
+                    'scan_id' =>$scan->id,
+                    'title'   =>$entry['title'],
+                    'results' =>$entry['results'],
+                    'violation_count'=>$violations_count_this_page,
+                    'warning_count'=>$warnings_count_this_page
+                ];
+                // Log::info('usage_after_datasets: ' . memory_get_peak_usage(true));
+                $chunks = array_chunk($pages, 1);
+                foreach ($chunks as $chunk) {
+                    Page::insert($chunk);
+                }
+                
+            }
+            
+        }
 
-                if($elements_violation > 0 && $page_has_violations == false){
-                    $page_has_violations = true;
-                    
-                }
-                
-                $total_warnings += $elements_warning;
-                $warnings_count_this_page += $elements_warning;
-                
-                if($elements_warning > 0 && $page_has_warnings == false){
-                    $page_has_warnings = true;
-                    
-                }
-            }
-            if($page_has_violations) {
-                $violations_pages++;
-            }
-            if($page_has_warnings) {
-                $warnings_pages++;
-            }
-            $pages[] = [
-                'scan_id' =>$scan->id,
-                'title'   =>$entry['title'],
-                'results' =>$entry['results'],
-                'violation_count'=>$violations_count_this_page,
-                'warning_count'=>$warnings_count_this_page
-            ];
-            
-        }
-        $chunks = array_chunk($pages, 20);
-        foreach ($chunks as $chunk) {
-            Page::insert($chunk);
-        }
-        // Log::info('usage_old' . memory_get_peak_usage(true));
         $scan->violation_count = $total_violations;
         $scan->warning_count = $total_warnings;
         $scan->violation_count_pages = $violations_pages;
         $scan->warning_count_pages = $warnings_pages;
 
-        
-
         $scan->save();#
+        // Log::info('usage_after_save: ' . memory_get_peak_usage(true));
         return response()->json([
             'message' => 'Import successful',
             'data' => $scan
